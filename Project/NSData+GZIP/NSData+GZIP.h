a93ed1e0915a429225731f0a72e7b4391d70bf8b
Rob Amos <bok@unsignedapps.com>
2016-01-18 14:22:00 +1100

    Add schema for library`

---
+++ Project/NSData+GZIP/NSData+GZIP.h
@@ -0,0 +1,19 @@
+//
+//  NSData+GZIP.h
+//  NSData+GZIP
+//
+//  Created by Robert Amos on 18/01/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSData+GZIP.
+FOUNDATION_EXPORT double NSData_GZIPVersionNumber;
+
+//! Project version string for NSData+GZIP.
+FOUNDATION_EXPORT const unsigned char NSData_GZIPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSData_GZIP/PublicHeader.h>
+
+
