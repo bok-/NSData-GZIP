9e2266559c13e483607b27c98e0e49345d48bb91
Rob Amos <bok@unsignedapps.com>
2016-02-11 14:41:23 +1100

    Add OSX Framework target

---
+++ Project/NSDataGZIP OSX/NSDataGZIP OSX.h
@@ -0,0 +1,19 @@
+//
+//  NSDataGZIP OSX.h
+//  NSDataGZIP OSX
+//
+//  Created by Robert Amos on 11/02/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NSDataGZIP OSX.
+FOUNDATION_EXPORT double NSDataGZIP_OSXVersionNumber;
+
+//! Project version string for NSDataGZIP OSX.
+FOUNDATION_EXPORT const unsigned char NSDataGZIP_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDataGZIP_OSX/PublicHeader.h>
+
+
